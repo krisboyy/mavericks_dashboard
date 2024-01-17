f62b6d569123e16537b5e383a5a01f90eac5f836
kris <nyk.krishnan@gmail.com>
2024-01-17 17:57:12 +0530

    Created initial design in Flutter. Testing i2c on Linux.

---
+++ lib/input_test.dart
@@ -0,0 +1,9 @@
+import 'package:dart_periphery/dart_periphery.dart';
+import 'dart:io';
+
+void main() {
+  var i2c = I2C(1);
+  try{
+    var pi = (i2c)
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,27 +8,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+    ]);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mavericks Dashboard',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,16 +26,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +33,120 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: EdgeInsets.all(0.03 * screenHeight),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/icons/indicator_left_off.png"),
+                  SizedBox(
+                    width: 0.5 * screenWidth,
+                    child: const Center(
+                      child: Text(
+                        "Please wear your seatbelt!",
+                        style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Image.asset("assets/icons/indicator_right_off.png")
+                ],
+              ),
+              SizedBox(
+                height: 0.05 * screenWidth,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Image.asset(
+                    'assets/compass.png',
+                    width: 0.35 * screenHeight,
+                  ),
+                  Image.asset(
+                    'assets/speedometer.png',
+                    height: 0.55 * screenHeight,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: 0.35 * screenHeight,
+                    child: Image.asset(
+                      'assets/car_topview.png',
+                      fit: BoxFit.fill,
+                      //width: 0.20 * screenHeight,
+                      //height: 0.31 * screenHeight,
+                    ),
+                  )
+                ],
+              ),
+              const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text(
+                  "[",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                    fontSize: 40,
+                  ),
+                ),
+                Text(
+                  "D",
+                  style: TextStyle(
+                    color: Color(0xFF0BE2FF),
+                    fontWeight: FontWeight.w900,
+                    fontSize: 40,
+                  ),
+                ),
+                Text(
+                  "]",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                    fontSize: 40,
+                  ),
+                )
+              ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/icons/seatbelt_off.png",
+                    height: 0.09 * screenHeight,
+                  ),
+                  const SizedBox(width: 20),
+                  Image.asset(
+                    "assets/icons/temperature_high.png",
+                    height: 0.09 * screenHeight,
+                  ),
+                  const SizedBox(width: 40),
+                  const Text(
+                    '69420\nkm',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 40,
+                    ),
+                  ),
+                  const SizedBox(width: 40),
+                  Image.asset(
+                    "assets/icons/handbrake_on.png",
+                    height: 0.09 * screenHeight,
+                  ),
+                  const SizedBox(width: 20),
+                  Image.asset(
+                    "assets/icons/highbeam_on.png",
+                    height: 0.09 * screenHeight,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
